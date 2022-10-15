b0e85be8d2c65f5bc9f0a865e6b77d24c2402312
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-10-15 15:53:44 +0100

    dev : fetch for data

---
+++ main.js
@@ -0,0 +1,31 @@
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector(".pokemon_image");
+const form = document.querySelector("form");
+const input = document.querySelector(".input_search");
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  );
+
+  const data = await APIResponse.json();
+  return data;
+};
+
+const renderPokemon = async (pokemon) => {
+  const data = await fetchPokemon(pokemon);
+  pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
+  pokemonImage.src =
+    data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+      "front_default"
+    ];
+  console.log(data);
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log(input.value);
+});
